e47a7f2b06cd3ea2fe46947f2a11720c07981ddc
Harshitha Mohan <harshitha.mohan@esko.com>
2024-03-26 12:23:59 +0530

    Version3

--- src/main/java/com/example/demo/CalculatorController.java
+++ src/main/java/com/example/demo/CalculatorController.java
@@ -1,24 +1,29 @@
 package com.example.demo;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 
 @Service
 @RestController
+@ControllerAdvice
 @ApiOperation("Operations on two numbers")
+@EnableGlobalMethodSecurity
 
 public class CalculatorController {
     
     
-    @PostMapping("/add")
+    @GetMapping("/add")
     public int add(@RequestParam int num1, @RequestParam int num2) {
+        System.out.println("Hellowworld!");
         return num1 + num2;
     }
     
@@ -40,10 +45,10 @@ public class CalculatorController {
         }
         return num1 / num2;
     }
-    
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST) 
+    public String handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ex.getMessage();
     }
 }
